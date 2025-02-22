0dc94113524ba031455f57a74dbd5394e6ddf1d0
Mi Mi Soe <mimisoe@acedatasystems.com>
2025-02-23 00:56:08 +0630

    -

---
+++ StudyPlannerApplication.App/StateContainer/NotificationStateContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace StudyPlannerApplication.App.StateContainer
+{
+    public class NotificationStateContainer
+    {
+        private int _notificationCount = 0;
+
+        public int NotificationCount
+        {
+            get => _notificationCount;
+            set
+            {
+                _notificationCount = value;
+                NotifyStateChanged();
+
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
